d27924f93d1740d59da7ddb9b2a95020d1b84abb
Gilmar Vogel <desenv2@idata.com.br>
2020-06-25 16:32:12 -0300

    make it better

---
+++ frontend/build/precache-manifest.cca6ae43af815467cf73e8fb5a9b1dd6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c016b46988389931d913e78be11bba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99f16c1755266917080f",
+    "url": "/static/css/main.2946d3bf.chunk.css"
+  },
+  {
+    "revision": "2ab89e071c80fb534371",
+    "url": "/static/js/2.b06fa9ce.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.b06fa9ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f16c1755266917080f",
+    "url": "/static/js/main.7d1f3bec.chunk.js"
+  },
+  {
+    "revision": "7bf6f6168c013be0eca2",
+    "url": "/static/js/runtime-main.8201579a.js"
+  },
+  {
+    "revision": "fe2283e1f17af7f6d2889b5692937968",
+    "url": "/static/media/home-background.fe2283e1.svg"
+  },
+  {
+    "revision": "47c4581a1dc536c1ba57fddafd6671b9",
+    "url": "/static/media/logo.47c4581a.svg"
+  }
+]);
